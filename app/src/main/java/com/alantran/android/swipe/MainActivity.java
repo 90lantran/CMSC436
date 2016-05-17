c978c35d38e861e7eabb92323d64945247adc1d1
Mike <marbukhm@yahoo.com>
2016-05-16 21:42:42 -0400

    no changes

--- app/src/main/java/com/alantran/android/swipe/MainActivity.java
+++ app/src/main/java/com/alantran/android/swipe/MainActivity.java
@@ -85,6 +85,8 @@ public class MainActivity extends AppCompatActivity implements
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(UserInfo.class);
+            startActivity(intent);
             return true;
         }
 
